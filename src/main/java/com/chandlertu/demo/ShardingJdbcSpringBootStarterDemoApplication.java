63a133131556a58aa0a9a3cae32b2b6d2c3e3834
Chandler Tu <chandlertu@chandlertu.com>
2018-06-12 14:11:41 +0800

    New Maven project

---
+++ src/main/java/com/chandlertu/demo/ShardingJdbcSpringBootStarterDemoApplication.java
@@ -0,0 +1,12 @@
+package com.chandlertu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJdbcSpringBootStarterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingJdbcSpringBootStarterDemoApplication.class, args);
+	}
+}
